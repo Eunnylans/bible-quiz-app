5011ce120dc1c5aa5407b83601d6811a8ee2d92a
Eunice Abieyuwa Igbinedion <eunice.igbinedion@yahoo.com>
2024-09-30 11:05:48 +0200

    Questions Component

---
+++ src/components/Questions/Questions.js
@@ -0,0 +1,34 @@
+// src/Questions.js
+
+const questions = [
+    {
+      questionText: 'What was the first miracle Jesus performed?',
+      answerOptions: [
+        { answerText: 'Turning water into wine', isCorrect: true },
+        { answerText: 'Feeding 5000 people', isCorrect: false },
+        { answerText: 'Walking on water', isCorrect: false },
+        { answerText: 'Raising Lazarus from the dead', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Who led the Israelites out of Egypt?',
+      answerOptions: [
+        { answerText: 'Joshua', isCorrect: false },
+        { answerText: 'David', isCorrect: false },
+        { answerText: 'Moses', isCorrect: true },
+        { answerText: 'Solomon', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'What is the first book of the Bible?',
+      answerOptions: [
+        { answerText: 'Genesis', isCorrect: true },
+        { answerText: 'Exodus', isCorrect: false },
+        { answerText: 'Psalms', isCorrect: false },
+        { answerText: 'Leviticus', isCorrect: false },
+      ],
+    },
+  ];
+  
+  export default questions;
+  
