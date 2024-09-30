d33bde57fe299754b8199487cd8018c3b2a4c8c8
Eunice Abieyuwa Igbinedion <eunice.igbinedion@yahoo.com>
2024-09-30 11:28:01 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React from 'react';
 import './App.css';
+import BibleQuiz from './components/BibleQuiz/BibleQuiz';
+import questions from './components/Questions/Questions'; // Import the questions
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bible Quiz</h1>
+      <BibleQuiz questions={questions} /> {/* Pass questions as props */}
     </div>
   );
 }
---
+++ src/components/BibleQuiz/BibleQuiz.js
@@ -0,0 +1,52 @@
+// src/BibleQuiz.js
+
+import React, { useState } from 'react';
+
+const BibleQuiz = ({ questions }) => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleAnswerClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  return (
+    <div className="quiz-container">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((option, index) => (
+              <button  key={index} onClick={() => handleAnswerClick(option.isCorrect)}>
+                {option.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default BibleQuiz;
